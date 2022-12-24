6761de51f291e5c6e5fdfa722fc69784bb20b71f
lokywolf2295 <matiasesek1995@gmail.com>
2022-12-24 11:36:40 -0300

    add Validation Method for Mail Password and Birth

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -11,10 +11,53 @@ export function valid(input){
   if(validators[typeOfInput]){
     validators[typeOfInput](input);
   }
+
+  if(input.validity.valid){
+    input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+  }else{
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = showMessageError(typeOfInput, input);
+  }
 }
+
+const bugType = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError"
+];
+
+const errorMessage = {
+  name:{
+    valueMissing: "El campo nombre no puede estar vacío"
+  },
+  email: {
+    valueMissing: "El campo correo no puede estar vacío",
+    typeMismatch: "El correo no es válido"
+  },
+  password:{
+    valueMissing: "El campo contraseña no puede estar vacío",
+    patternMismatch: "Almenos 6 caracteres, maximo 12 caracteres, debe contener una letra mayúscula, debe contener una letra minúscula, debe contener un número, debe contener un caracter especial !¡@#$%&.,_- y no puede contener espacios"
+  },
+  birth:{
+    valueMissing: "Este campo no puede estar vacío",
+    customError: "Debes tener al menos 18 años de edad"
+  },
+};
 
 const validators ={
     birth: input=> validateBirth(input),
+}
+
+function showMessageError(typeOfInput, input){
+  let message = "";
+  bugType.forEach(error => {
+    if(input.validity[error]){
+      message = errorMessage[typeOfInput][error];
+    }
+  })
+  return message;
 }
 
 function validateBirth(input) {
